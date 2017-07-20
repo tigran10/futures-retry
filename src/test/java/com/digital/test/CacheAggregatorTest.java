4a8d062da7f5b23af0596bcd504ca35d3aa197a5
tigran <t.mnatsakanyan@gmail.com>
2017-07-20 17:19:02 +0100

    tm - some tests

--- src/test/java/com/digital/test/CacheAggregatorTest.java
+++ src/test/java/com/digital/test/CacheAggregatorTest.java
@@ -1,11 +1,193 @@
 package com.digital.test;
 
-import static org.junit.Assert.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
+import org.fest.assertions.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CacheAggregatorTest {
-    @org.junit.Test
-    public void get() throws Exception {
+
+
+    @Mock
+    private CacheFoo fooCache;
+
+    @Mock
+    private CacheBar barCache;
+
+    @Mock
+    private Logger logger;
+
+
+    private CacheAggregator cache ;
+
+    @Before
+    public void init() {
+        given(fooCache.name()).willReturn("fooCache");
+        given(barCache.name()).willReturn("barCache");
+        cache = new CacheAggregator(fooCache, barCache) {
+            @Override
+            public Logger getLogger() {
+                return logger;
+            }
+        };
+    }
+
+
+    @Test
+    public void testCacheAggregatorGetReturnsFirstAvailableValue() {
+        //given
+        given(fooCache.get(anyString())).willReturn("apple");
+        given(barCache.get(anyString())).willReturn("carrot");
+
+
+        //when
+        cache.get("random");
+
+        //then
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(fooCache, atLeast(1)).get(argumentCaptor.capture());
+        verify(barCache, atLeast(1)).get(argumentCaptor.capture());
+    }
+
+    @Test
+    public void testCacheAggregatorTouchesEachCacheOnce() {
+        given(fooCache.get(anyString())).willReturn("carrot");
+        given(barCache.get(anyString())).willReturn("carrot");
+
+        assertThat(cache.get("random")).isEqualTo("carrot");
+    }
+
+
+    @Test
+    public void testCacheAggregatorGetReturnsFirstNotNullValue() {
+        given(fooCache.get(anyString())).willReturn(null);
+        given(barCache.get(anyString())).willReturn("carrot");
+
+        assertThat(cache.get("random")).isEqualTo("carrot");
+    }
+
+    @Test
+    public void testCacheAggregatorGetReturnsFirstNotNullValueEvenIfOneOfThemDies() {
+        given(fooCache.get(anyString())).willThrow(new RuntimeException("i am dead"));
+        given(barCache.get(anyString())).willReturn("carrot");
+
+        assertThat(cache.get("random")).isEqualTo("carrot");
+    }
+
+    @Test
+    public void testCacheAggregatorCallsSuccessCallbackOnEachCache() {
+
+        //given
+        given(fooCache.get(anyString())).willReturn("carrot");
+        given(barCache.get(anyString())).willReturn("apple");
+        
+
+        //when
+        cache.get("random");
+
+        //then
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(logger, atLeast(2)).log(argumentCaptor.capture());
+
+        //and then
+        List<String> values = argumentCaptor.getAllValues();
+
+        assertThat(values).contains("trying to get key randomfrom cache name fooCache");
+        assertThat(values).contains("trying to get key randomfrom cache name barCache");
+
+        assertThat(values).contains("wohoo got value from cache: fooCache");
+        assertThat(values).contains("wohoo got value from cache: barCache");
+
+    }
+
+    @Test
+    public void testCacheAggregatorCallsFailureCallbackOnEachCache() {
+
+        //given
+        given(fooCache.get(anyString())).willThrow(new RuntimeException("i am dead"));
+        given(barCache.get(anyString())).willThrow(new RuntimeException("i am dead"));
+        
+
+        //when
+        cache.get("random");
+
+        //then
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(logger, atLeast(2)).log(argumentCaptor.capture());
+
+
+        //and then
+        List<String> values = argumentCaptor.getAllValues();
+
+        assertThat(values).contains("trying to get key randomfrom cache name fooCache");
+        assertThat(values).contains("trying to get key randomfrom cache name barCache");
+        assertThat(values).contains("holly crap, just failed on getting value from cache: fooCache");
+        assertThat(values).contains("holly crap, just failed on getting value from cache: barCache");
+
+    }
+
+    @Test
+    public void testCacheAggregatorCallsFailureAndSuccessCallbacks() {
+
+        //given
+        given(fooCache.get(anyString())).willThrow(new RuntimeException("i am dead"));
+        given(barCache.get(anyString())).willReturn("I am not");
+        
+
+
+        //when
+        cache.get("random");
+
+        //then
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(logger, atLeast(2)).log(argumentCaptor.capture());
+
+
+        //and then
+        List<String> values = argumentCaptor.getAllValues();
+
+        assertThat(values).contains("trying to get key randomfrom cache name fooCache");
+        assertThat(values).contains("trying to get key randomfrom cache name barCache");
+        assertThat(values).contains("holly crap, just failed on getting value from cache: fooCache");
+        assertThat(values).contains("wohoo got value from cache: barCache");
+
+    }
+
+    @Test
+    public void testCacheAggregatorCallsWarningLogsWhenInconsistentResultsAreRecorded() {
+
+        //given
+        given(fooCache.get(anyString())).willReturn("apple");
+        given(barCache.get(anyString())).willReturn("carrot");
+        
+
+
+        //when
+        cache.get("random");
+
+        //then
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(logger, atLeast(1)).log(argumentCaptor.capture());
+
+
+        //and then
+        List<String> values = argumentCaptor.getAllValues();
+
+        assertThat(values).contains("world is very inconsistent");
+
     }
 
 }
